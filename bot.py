3271aaf3eb16f80c82ff731c5b499016e9bc9cf8
Jeff Lu <jylu@gogoair.com>
2016-06-29 14:05:48 -0500

    Project files

---
+++ bot.py
@@ -0,0 +1,28 @@
+"""time needed for sleep, datetime needed to get current datetime"""
+import random
+import datetime
+
+import dateutil.parser as parser
+from questions import questions
+from errbot import BotPlugin
+
+class CitizenshipTest(BotPlugin):  # pylint: disable=too-many-ancestors
+
+    def idle_check(self):
+        """Instance check for idle 24 hours"""
+        start_min = datetime.date.today().isoformat() + "T14:00:00"
+        start_max = datetime.date.today().isoformat() + "T22:00:00"
+        # If time is between 4 and 4:01, run the function.
+        if parser.parse(start_min) < datetime.datetime.now() < parser.parse(start_max):
+            self.quiz("<#C1MFBRH3L>")
+
+    def quiz(self, channel):
+        room = self.build_identifier(channel)
+
+        num = random.random()
+        if num <= .5:
+            self.send(room, random.choice(questions))
+
+    def activate(self):
+        super().activate()
+        self.start_poller(1500, self.idle_check)
